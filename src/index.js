1afb6d1d1a753c475dc6746f2f0c03fa84d1accc
Ryland Taniguchi <ryland0000@gmail.com>
2022-04-17 09:12:26 +0100

    control flow logic

--- src/index.js
+++ src/index.js
@@ -1,71 +1,74 @@
-// Remember the acronym UPS check
+// // Remember the acronym UPS check
 
-// Step One (Understand). We have to understand the problem.
-// 1. What is the unknown?
+// // Step One (Understand). We have to understand the problem.
+// // 1. What is the unknown?
 
-find the first pair of intergers where the sum is zero
-return an array that includes both intergers
+// find the first pair of intergers where the sum is zero
+// return an array that includes both intergers
 
-// 2. What are the data inputs?
-sorted array of intergers
+// // 2. What are the data inputs?
+// sorted array of intergers
 
-// 3. Can you restate the problem in your own words?
-find the FIRST pair that equals zero when added together
+// // 3. Can you restate the problem in your own words?
+// find the FIRST pair that equals zero when added together
 
-// 4. Can the unknown be determined from the data inputs?
-yes 
-// 5. Is the data inputs sufficient to determine the unknown? Insufficient? Redundant? Contradictory? 
+// // 4. Can the unknown be determined from the data inputs?
+// yes
+// // 5. Is the data inputs sufficient to determine the unknown? Insufficient? Redundant? Contradictory?
 
-sufficient 
+// sufficient
 
-// 6. How should I label important pieces of data input that are a part of the problem?
-arr
+// // 6. How should I label important pieces of data input that are a part of the problem?
+// arr
 
-// 7. Draw a figure. Introduce suitable notation.
-// 8. Separate the various parts of the condition. Can you write them down?
+// // 7. Draw a figure. Introduce suitable notation.
+// // 8. Separate the various parts of the condition. Can you write them down?
 
-// Step Two (Plan). Come up with concrete examples to help you understand the problem better. Find a connection between the data inputs and the unknown. You may be obliged to consider auxiliary problems if an immediate connection cannot be found. 
+// // Step Two (Plan). Come up with concrete examples to help you understand the problem better. Find a connection between the data inputs and the unknown. You may be obliged to consider auxiliary problems if an immediate connection cannot be found.
 
-// 9.  Have you seen it before? Or have you seen the same problem in a slightly different form? Do you know a related problem? 
+// // 9.  Have you seen it before? Or have you seen the same problem in a slightly different form? Do you know a related problem?
 
-// 10.   Do you know a coding strategy that could be useful? Look at the unknown! And try to think of a familiar problem having the same or a similar unknown. Here is a problem related to yours and solve before. Could you use it? Could you use its result? Could you use its method? 
+// // 10.   Do you know a coding strategy that could be useful? Look at the unknown! And try to think of a familiar problem having the same or a similar unknown. Here is a problem related to yours and solve before. Could you use it? Could you use its result? Could you use its method?
 
-multiple pointer CanvasPattern
+// multiple pointer CanvasPattern
 
-// 11.    Should you introduce some auxiliary element in order to make its use possible? 
-// 12. Start with a simple example. Could you restate the problem? Could you restate it still differently? 
+// // 11.    Should you introduce some auxiliary element in order to make its use possible?
+// // 12. Start with a simple example. Could you restate the problem? Could you restate it still differently?
 
-console.log(sumZero([-3, -2, -1, 0, 1, 2, 3]))
+// console.log(sumZero([-3, -2, -1, 0, 1, 2, 3]))
 
-return [3, -3]
+// return [3, -3]
 
-// 13. What about examples with empty inputs? Any other edge case examples? What examples with invalid inputs? 
-// 14. Progress to more complex examples. What is your updated plan here to find a solution?
-// 15. If you still cannot solve the proposed problem, try to solve first some related problem. Could you imagine a more accessible related problem? A more general problem? A more special problem? An analogous problem? Could you solve a part of the problem? Keep only a part of the data inputs, drop the other part; how far is the unknown then determined, how can it vary? Could you derive something useful from the data? Could you think of other data appropriate to determine the unknown? Could you change the unknown or the data, or both if necessary, so that the new unknown and the new data are nearer to each other? Did you use all the data? Did you use the whole of the data inputs? Have you taken into account all essential notions involved in the problem?
+// // 13. What about examples with empty inputs? Any other edge case examples? What examples with invalid inputs?
+// // 14. Progress to more complex examples. What is your updated plan here to find a solution?
+// // 15. If you still cannot solve the proposed problem, try to solve first some related problem. Could you imagine a more accessible related problem? A more general problem? A more special problem? An analogous problem? Could you solve a part of the problem? Keep only a part of the data inputs, drop the other part; how far is the unknown then determined, how can it vary? Could you derive something useful from the data? Could you think of other data appropriate to determine the unknown? Could you change the unknown or the data, or both if necessary, so that the new unknown and the new data are nearer to each other? Did you use all the data? Did you use the whole of the data inputs? Have you taken into account all essential notions involved in the problem?
 
+// // Step Three (Solve). Carry out your plan of the solution and check each one of your steps in pseudocode.
+// // 16.  Can you see clearly that the step is correct?
 
-// Step Three (Solve). Carry out your plan of the solution and check each one of your steps in pseudocode.
-// 16.  Can you see clearly that the step is correct?
+// pseudocode below
 
-pseudocode below
-
-// IV. 	Step Four (Check). Examine the solution obtained and refactor. 
-// 17.  Can you check your result? 
-// 18.  Can you check the argument? 
-// 19.  Can you derive the result differently? 
-// 20.  Can you see it at a glance? 
-// 21.  Can you make the code DRYer and refactor? 
-// 22.  Can you improve the performance? 
+// IV. 	Step Four (Check). Examine the solution obtained and refactor.
+// 17.  Can you check your result?
+// 18.  Can you check the argument?
+// 19.  Can you derive the result differently?
+// 20.  Can you see it at a glance?
+// 21.  Can you make the code DRYer and refactor?
+// 22.  Can you improve the performance?
 // 23.   How have other people solved this problem?
 
+console.log(sumZero([-3, -2, -1, 0, 1, 2, 3]));
 
-function sumZero(arr){
+function sumZero(arr) {
   let sumZeroArr = [];
   // find the first pair of intergers that equal 0 when added (for loop)
   for (let i = 0; i < arr.length; i++) {
-
+    for (let j = i; j < arr.length; j--) {
+      if (arr[i] === arr[j]) {
+        sumZeroArr.push(arr[i], arr[j]);
+      }
+    }
   }
-
   // return sumZeroArr
   return sumZeroArr;
 }
