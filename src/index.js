53d5f9ceca287ae925a6f6ede086749e1136b0f0
Ryland Taniguchi <ryland0000@gmail.com>
2022-04-17 10:43:19 +0100

    multiple pointer strategy

--- src/index.js
+++ src/index.js
@@ -66,11 +66,24 @@ console.log(sumZero([-3, -2, -1, 0, 1, 2, 3]));
 
 function sumZero(arr) {
   // find the first pair of intergers that equal 0 when added (for loop)
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] + arr[j] === 0) {
-        return [arr[i], arr[j]];
-      }
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
     }
   }
 }
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[i] + arr[j] === 0) {
+//         return [arr[i], arr[j]];
+//       }
+//     }
+//   }
+// }
